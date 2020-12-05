399188d354907c74da2e27efd0cbfad7490c3117
LaurelAnderson <52584596+LaurelAnderson@users.noreply.github.com>
2020-12-05 12:48:32 -0800

    Worked on login.js
    Added some tests and worked on the login page.

--- App/Login.js
+++ App/Login.js
@@ -19,44 +19,50 @@ class UserList {
     return this.listArr.find((element) => element === user);
   }
 }
-// User input
 
-const loginForm = window.document.getElementById('login-form');
-const loginButton = window.document.getElementById('login-form-submit');
-
-loginButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  const username = loginForm.username.value;
-  const password = loginForm.password.value;
-
-  // make a user list object
-  const listOfUsers = new UserList();
-
-  // make a user profile object
-  const user = new UserProfile(username, password);
-
-  if (listOfUsers.search(user)) {
-    alert('you are a user.');
+function userInput(listOfUsers, profile) {
+  const retProfile = listOfUsers.search(profile);
+  if (retProfile) {
+    console.log('you are a user.');
+    console.log(listOfUsers);
+    if (retProfile.password === profile.password && retProfile.username === profile.username) {
+      console.log('you are signed in!');
+    } else {
+      console.log('incorrect username or password');
+    }
   } else {
-    alert('we made an account for you.');
-    listOfUsers.addUser(user);
+    console.log('we made an account for you.');
+    listOfUsers.addUser(profile);
+    console.log(listOfUsers);
   }
+}
 
-  // if (username === 'lil_laurel' && password === 'password123') {
-  //   alert('You have successfully logged in.');
-  //   window.location.reload();
-  // } else {
-  //   alert('Invalid username and/or password');
-  // }
-});
+const user1 = new UserProfile('Me', 'Password123');
+const user2 = new UserProfile('Laurel', 'Giveiturall');
+const user3 = new UserProfile('Irina', 'CoolestCat');
+const listOfUsers = new UserList();
+listOfUsers.addUser(user1);
+listOfUsers.addUser(user2);
 
-// const user1 = new UserProfile('Me', 'Password123');
-// const user2 = new UserProfile('Laurel', 'Giveiturall');
-// const user3 = new UserProfile('Irina', 'CoolestCat');
-//
-// console.log(user1 + user2);
-// const listOfUsers = new UserList();
-// listOfUsers.addUser(user1);
-// listOfUsers.addUser(user2);
+userInput(listOfUsers, user2);
+
 // console.log(listOfUsers);
-// console.log(listOfUsers.search(user2).password);
+// console.log(listOfUsers.search(user3).password);
+
+//
+
+// const loginForm = window.document.getElementById('login-form');
+// const loginButton = window.document.getElementById('login-form-submit');
+
+// loginButton.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const username = loginForm.username.value;
+//   const password = loginForm.password.value;
+
+// if (username === 'lil_laurel' && password === 'password123') {
+//   alert('You have successfully logged in.');
+//   window.location.reload();
+// } else {
+//   alert('Invalid username and/or password');
+// }
+// });
