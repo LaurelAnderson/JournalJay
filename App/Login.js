447c756fbf58de82db0b625761d2eb8f9a9cc959
LaurelAnderson <52584596+LaurelAnderson@users.noreply.github.com>
2020-12-05 17:18:44 -0800

    Made tests for login screen
    I am working on making the page save the usernames and passwords to cookies.

--- App/Login.js
+++ App/Login.js
@@ -1,3 +1,4 @@
+// This is what makes the user profile objects.
 // eslint-disable-next-line max-classes-per-file
 class UserProfile {
   constructor(name, password) {
@@ -6,6 +7,7 @@ class UserProfile {
   }
 }
 
+// This class makes an array of users and can add a user and search the array using a username.
 class UserList {
   constructor() {
     this.listArr = [];
@@ -15,22 +17,22 @@ class UserList {
     this.listArr.push(user);
   }
 
-  search(user) {
-    return this.listArr.find((element) => element === user);
+  search(username) {
+    return this.listArr.find((element) => element.name === username);
   }
 }
 
+// This is a function that simulates how the controller will interact with the view.
 function userInput(listOfUsers, profile) {
-  const retProfile = listOfUsers.search(profile);
-  if (retProfile) {
+  const retProfile = listOfUsers.search(profile.name);
+  if (retProfile) { // If they are a known user
     console.log('you are a user.');
-    console.log(listOfUsers);
-    if (retProfile.password === profile.password && retProfile.username === profile.username) {
+    if (retProfile.password === profile.password && retProfile.username === profile.username) { // check password
       console.log('you are signed in!');
-    } else {
+    } else { // incorrect password
       console.log('incorrect username or password');
     }
-  } else {
+  } else { // if they are not a known user, make an account for them.
     console.log('we made an account for you.');
     listOfUsers.addUser(profile);
     console.log(listOfUsers);
@@ -40,29 +42,26 @@ function userInput(listOfUsers, profile) {
 const user1 = new UserProfile('Me', 'Password123');
 const user2 = new UserProfile('Laurel', 'Giveiturall');
 const user3 = new UserProfile('Irina', 'CoolestCat');
+const user4 = new UserProfile('Me', 'notpassword123');
 const listOfUsers = new UserList();
 listOfUsers.addUser(user1);
 listOfUsers.addUser(user2);
-
-userInput(listOfUsers, user2);
-
-// console.log(listOfUsers);
-// console.log(listOfUsers.search(user3).password);
-
-//
+listOfUsers.addUser(user3);
+console.log(listOfUsers);
+userInput(listOfUsers, user4);
 
 // const loginForm = window.document.getElementById('login-form');
 // const loginButton = window.document.getElementById('login-form-submit');
-
+//
 // loginButton.addEventListener('click', (e) => {
 //   e.preventDefault();
 //   const username = loginForm.username.value;
 //   const password = loginForm.password.value;
-
-// if (username === 'lil_laurel' && password === 'password123') {
-//   alert('You have successfully logged in.');
-//   window.location.reload();
-// } else {
-//   alert('Invalid username and/or password');
-// }
+//
+//   if (username === 'lil_laurel' && password === 'password123') {
+//     alert('You have successfully logged in.');
+//     window.location.reload();
+//   } else {
+//     alert('Invalid username and/or password');
+//   }
 // });
--- App/LoginTest.js
+++ App/LoginTest.js
@@ -1,9 +1,29 @@
-/*  global UserProfile */
+/*  global UserProfile, UserList */
 describe('Test for Correctness', function () {
   describe('UserProfile()', function () {
     const user1 = new UserProfile('Me', 'Password123');
+
     it('Created a user profile!', function () {
-      chai.expect(UserProfile(user1)).to.deep.equal(user1);
+      chai.expect(new UserProfile('Me', 'Password123')).to.deep.equal(user1);
+    });
+  });
+  describe('UserList()', function () {
+    const user1 = new UserProfile('Me', 'Password123');
+    const user2 = new UserProfile('Laurel', 'Giveiturall');
+    const user3 = new UserProfile('Irina', 'CoolestCat');
+    const listOfUsers = new UserList();
+    const listOfUsers2 = new UserList();
+    listOfUsers.addUser(user1);
+    listOfUsers.addUser(user2);
+    listOfUsers2.addUser(user1);
+    listOfUsers2.addUser(user2);
+
+    it('Added users to the user list!', function () {
+      chai.expect(listOfUsers).to.deep.equal(listOfUsers2);
+    });
+
+    it('Searched a user by username!', function () {
+      chai.expect(listOfUsers.search(user1.name)).to.deep.equal(user1);
     });
   });
 });
