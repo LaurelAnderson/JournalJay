9b7e1aefe5440d36f7feb189accfec2eca0ec230
LaurelAnderson <52584596+LaurelAnderson@users.noreply.github.com>
2020-12-16 15:49:18 -0800

    Fixed some some eslint issue
    Fixed some eslint issues.

--- App/Entry.js
+++ App/Entry.js
@@ -1,11 +1,48 @@
+/* global Entry */
+
 // reference to save button
 const saveButton = document.getElementById('saveButton');
 // global entry variable
-const entries = '';
+// const entries = '';
 // get today's date
 const today = (new Date()).setHours(0, 0, 0, 0);
-// initialize entries
-init();
+
+// go to "write" div when writing entries
+function writeEntry() {
+  document.getElementById('peruse').style.display = 'none';
+  document.getElementById('write').style.display = 'block';
+  document.getElementById('entryTitle').value = '';
+  document.getElementById('entryBody').value = '';
+  document.getElementById('entryMood').value = '';
+
+  document.getElementById('helpButton').addEventListener('click', function (e) {
+    window.location.href = 'help.html';
+  });
+
+  document.getElementById('profButton').addEventListener('click', function (e) {
+    window.location.href = 'profile.html';
+  });
+
+  document.getElementById('cancelButton').addEventListener('click', function (e) {
+    window.location.href = 'view.html';
+  });
+}
+
+// go to "peruse" div when reading entries
+function readEntries() {
+  document.getElementById('peruse').style.display = 'block';
+  document.getElementById('write').style.display = 'none';
+}
+
+// show the user the entry they click on when in "peruse" mode
+function displayEntry(entry) {
+  // get array from memory
+  const entryArray = JSON.parse(localStorage.getItem('entryData'));
+  let out = `<h2>${entryArray[entry].title.concat(` ${entryArray[entry].mood}`)}</h2>`;
+  out += `<h4>Date: ${new Date(entryArray[entry].date).toDateString()}</h4>`;
+  out += `<p>${entryArray[entry].body}</p>`;
+  document.getElementById('entryDisplay').innerHTML = out;
+}
 
 // initialize the screen; update the screen for the user
 function init() {
@@ -14,14 +51,14 @@ function init() {
   const dates = [];
 
   // if there are journal entries in local storage
-  if (entryArray != null && entryArray != '') {
+  if (entryArray != null && entryArray !== '') {
     // make an array of all entries
     entryArray = JSON.parse(localStorage.getItem('entryData'));
     // loop through the array to display
     for (let x = 0; x < entryArray.length; x++) {
       out += `<option value=${x}>`;
       out += entryArray[x].title;
-      out += " " + entryArray[x].mood;
+      out += ` ${entryArray[x].mood}`;
       out += '</option>';
       document.getElementById('entryMaster').innerHTML = out;
       dates.push(entryArray[x].date);
@@ -31,7 +68,7 @@ function init() {
       if (dates.includes(today)) {
         window.alert('Sorry, you have already made a journal entry today.\nView old entries and come back tomorrow!');
         document.getElementById('peruseButton').disabled = true;
-      } else { writeEntry(); } 
+      } else { writeEntry(); }
     });
 
     document.getElementById('entryMaster').addEventListener('click', function (e) {
@@ -50,52 +87,8 @@ function init() {
   } else { writeEntry(); }
 }
 
-// go to "write" div when writing entries
-function writeEntry() {
-  document.getElementById('peruse').style.display = 'none';
-  document.getElementById('write').style.display = 'block';
-  document.getElementById('entryTitle').value = '';
-  document.getElementById('entryBody').value = '';
-  document.getElementById('entryMood').value = '';
-
-  document.getElementById('helpButton').addEventListener('click', function (e) {
-      window.location.href = 'help.html';
-  });
-
-  document.getElementById('profButton').addEventListener('click', function (e) {
-      window.location.href = 'profile.html';
-  });
-
-  document.getElementById('cancelButton').addEventListener('click', function (e) {
-    window.location.href = 'view.html';
-});
-}
-
-// go to "peruse" div when reading entries
-function readEntries() {
-  document.getElementById('peruse').style.display = 'block';
-  document.getElementById('write').style.display = 'none';
-}
-
-// show the user the entry they click on when in "peruse" mode
-function displayEntry(entry) {
-  // get array from memory
-  const entryArray = JSON.parse(localStorage.getItem('entryData'));
-  let out = `<h2>${entryArray[entry].title.concat(" " + entryArray[entry].mood)}</h2>`;
-  out += `<h4>Date: ${new Date(entryArray[entry].date).toDateString()}</h4>`;
-  out += `<p>${entryArray[entry].body}</p>`;
-  document.getElementById('entryDisplay').innerHTML = out;
-}
-
-// save the entry the user made
-saveButton.onclick = function () {
-  const entryDate = new Date().setHours(0, 0, 0, 0);
-  const entryTitle = document.getElementById('entryTitle').value;
-  const entryBody = document.getElementById('entryBody').value;
-  const entryMood = document.getElementById('entryMood').options[document.getElementById('entryMood').selectedIndex].text;
-  const theEntry = new Entry(entryTitle, entryBody, entryDate, entryMood);
-  saveEntry(theEntry);
-};
+// initialize entries
+init();
 
 // helper function for save button
 function saveEntry(entry) {
@@ -106,3 +99,14 @@ function saveEntry(entry) {
   readEntries();
   init();
 }
+
+// save the entry the user made
+saveButton.onclick = function () {
+  const entryDate = new Date().setHours(0, 0, 0, 0);
+  const entryTitle = document.getElementById('entryTitle').value;
+  const entryBody = document.getElementById('entryBody').value;
+  const entryMood = document.getElementById('entryMood').options[
+    document.getElementById('entryMood').selectedIndex].text;
+  const theEntry = new Entry(entryTitle, entryBody, entryDate, entryMood);
+  saveEntry(theEntry);
+};
